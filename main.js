b8099e668190acafad86e496cb577b87edc7e536
Tjaden Hess <tjade273>
2016-04-18 17:28:05 -0700

    fixed typo

--- main.js
+++ main.js
@@ -1,9 +1,9 @@
 var password=prompt("Password: ");
 var siteSalt=prompt("Site Salt: ");
 
-var salt = "950130eec143886e41cf056ecbab427b3e9487fabdd6ced6845d599d4fa09aa5"
+var salt = "950130eec143886e41cf056ecbab427b3e9487fabdd6ced6845d599d4fa09aa5";
 
-var stretched = CryptoJS.PBKDF2.(password,salt,{keySize: 256/32});
+var stretched = CryptoJS.PBKDF2(password,salt,{keySize: 256/32});
 
 var hashedPass = CryptoJS.sha256(stretched+siteSalt);
 
